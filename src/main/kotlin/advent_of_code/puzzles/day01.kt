4fffd182fff4ed752af8aea19b606f1808a2b6d2
Johan Garcia <john.jgp@gmail.com>
2024-12-01 16:29:13 -0300

    solved part 1

--- src/main/kotlin/advent_of_code/puzzles/day01.kt
+++ src/main/kotlin/advent_of_code/puzzles/day01.kt
@@ -3,19 +3,53 @@ package advent_of_code.puzzles
 import advent_of_code.utils.readFile
 
 fun main() {
-    historianHysteria()
+    historianHysteriaA()
 }
 
-fun historianHysteria() {
+fun historianHysteriaA() {
     val input = readFile("src/main/resources/inputs/day_01_input.txt")
     val (locationX, locationY) = input.split("\n")
-        .fold(Pair(mutableListOf<String>(), mutableListOf<String>())) { acc, line ->
+        .fold(Pair(mutableListOf<Int>(), mutableListOf<Int>())) { acc, line ->
             line.split("   ").let {
-                acc.first.addLast(it[0])
-                acc.second.addLast(it[1])
+                acc.first.addLast(it[0].toInt())
+                acc.second.addLast(it[1].toInt())
             }
             acc
         }
-    println(locationX.last())
-    println(locationY.last())
+    locationX.sort()
+    locationY.sort()
+    locationX.foldIndexed(0) { index, acc, curr ->
+        val distance = curr - locationY[index]
+        if (distance < 0) {
+            acc + (distance * -1)
+        } else {
+            acc + distance
+        }
+    }.let {
+        println(it)
+    }
+}
+
+fun historianHysteriaB() {
+    val input = readFile("src/main/resources/inputs/day_01_input.txt")
+    val (locationX, locationY) = input.split("\n")
+        .fold(Pair(mutableListOf<Int>(), mutableListOf<Int>())) { acc, line ->
+            line.split("   ").let {
+                acc.first.addLast(it[0].toInt())
+                acc.second.addLast(it[1].toInt())
+            }
+            acc
+        }
+    locationX.sort()
+    locationY.sort()
+    locationX.foldIndexed(0) { index, acc, curr ->
+        val distance = curr - locationY[index]
+        if (distance < 0) {
+            acc + (distance * -1)
+        } else {
+            acc + distance
+        }
+    }.let {
+        println(it)
+    }
 }
