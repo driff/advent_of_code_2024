8dce254636824e229985524f3d0f85ea02df435d
Johan Garcia <john.jgp@gmail.com>
2024-12-01 23:08:35 -0300

    done with day 1

--- src/main/kotlin/advent_of_code/puzzles/day01.kt
+++ src/main/kotlin/advent_of_code/puzzles/day01.kt
@@ -4,6 +4,7 @@ import advent_of_code.utils.readFile
 
 fun main() {
     historianHysteriaA()
+    historianHysteriaB()
 }
 
 fun historianHysteriaA() {
@@ -26,30 +27,29 @@ fun historianHysteriaA() {
             acc + distance
         }
     }.let {
-        println(it)
+        println("part a -> $it")
     }
 }
 
 fun historianHysteriaB() {
     val input = readFile("src/main/resources/inputs/day_01_input.txt")
-    val (locationX, locationY) = input.split("\n")
-        .fold(Pair(mutableListOf<Int>(), mutableListOf<Int>())) { acc, line ->
+    val rightList = mutableListOf<Int>()
+    val map = input.split("\n")
+        .fold(mutableMapOf<Int, Int>()) { acc, line ->
             line.split("   ").let {
-                acc.first.addLast(it[0].toInt())
-                acc.second.addLast(it[1].toInt())
+                acc[it[0].toInt()] = 0
+                rightList.addLast(it[1].toInt())
             }
             acc
         }
-    locationX.sort()
-    locationY.sort()
-    locationX.foldIndexed(0) { index, acc, curr ->
-        val distance = curr - locationY[index]
-        if (distance < 0) {
-            acc + (distance * -1)
-        } else {
-            acc + distance
+    rightList.forEach {
+        map[it]?.let { curr ->
+            map[it] = curr+1
         }
-    }.let {
-        println(it)
     }
+    var count = 0
+    map.forEach { (key, value) ->
+        count += key * value
+    }
+    println("part b -> $count")
 }
